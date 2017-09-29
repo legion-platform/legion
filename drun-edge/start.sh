acf3e980eb0d9c0668217c662603a8bf153c2811
Alexey Kharlamov <alexey@kharlamov.biz>
2017-09-29 17:07:02 +0100

    NginX replaced with OpenResty/LuaJIT

---
+++ drun-edge/start.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env sh
+
+/usr/local/openresty/bin/openresty
+consul-template -template="/nginx.conf.ctmpl:/usr/local/openresty/nginx/conf/nginx.conf:/usr/local/openresty/bin/openresty -s reload"
