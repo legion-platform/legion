d5b8cdc7485e6ae9ed1ccbdead4e46294be0fc3b
Kirill Makhonin <kroks.rus@gmail.com>
2017-10-08 16:36:59 +0300

    Add Colors (console symbols for colorize output) to utils

--- drun/drun/utils.py
+++ drun/drun/utils.py
@@ -79,3 +79,17 @@ class TemporaryFolder:
         :return: None
         """
         self.remove()
+
+
+class Colors:
+    """
+    Terminal colors
+    """
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
