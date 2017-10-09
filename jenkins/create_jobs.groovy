a46444c9e4495009bd0f24dd8a0c04f8b1fd5c31
Kirill Makhonin <kroks.rus@gmail.com>
2017-10-09 21:11:34 +0300

    Move Jenkinsfile to Jenkins Job directly

--- jenkins/create_jobs.groovy
+++ jenkins/create_jobs.groovy
@@ -3,8 +3,7 @@
 import jenkins.model.*
 import hudson.plugins.git.*
 import org.jenkinsci.plugins.workflow.job.WorkflowJob
-import org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition
-import hudson.triggers.*
+import org.jenkinsci.plugins.workflow.cps.CpsFlowDefinition
 import com.cloudbees.plugins.credentials.*
 import com.cloudbees.plugins.credentials.common.*
 import com.cloudbees.plugins.credentials.domains.*
@@ -32,25 +31,11 @@ if (store.getCredentials(domain).size() == 0){
 // Create job
 
 name = "DRUN-examples";
-path = "jenkins/Jenkinsfile";
-git = "git@github.com:akharlamov/drun-root.git";
-triggerSchedule = "H/15 * * * *";
 
 if (Jenkins.instance.getJobNames().size() == 0){
-	userRemoteConfigs = [new UserRemoteConfig(git, null, null, "drun-root-key")];
-	branches = [new BranchSpec("*/master")];
-	scm = new GitSCM(userRemoteConfigs, 
-		branches, 
-		false, 
-		[],
-		null, null, 
-		[]);
-
-	trigger = new SCMTrigger(triggerSchedule);
 
 	job = new WorkflowJob(Jenkins.instance, name);
-	job.definition = new CpsScmFlowDefinition(scm, path);
-	job.addTrigger(trigger)
+	job.definition = new CpsFlowDefinition(new File('/usr/share/jenkins/initial-job').text, false);
 
 	Jenkins.instance.add(job, name);
 }
