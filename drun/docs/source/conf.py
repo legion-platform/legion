ad5401de607ec09dd47b05c5bd11e7e380c41a73
Kirill Makhonin <Kirill_Makhonin@epam.com>
2017-10-17 16:30:31 +0300

    Add "view code" link in documentation

--- drun/docs/source/conf.py
+++ drun/docs/source/conf.py
@@ -33,6 +33,7 @@ sys.path.insert(0, os.path.abspath('../..'))
 # ones.
 extensions = ['sphinx.ext.autodoc',
     'sphinx.ext.todo',
+    'sphinx.ext.viewcode',
     'sphinx.ext.imgmath']
 
 # Add any paths that contain templates here, relative to this directory.
