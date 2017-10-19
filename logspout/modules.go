48ba95d48ebe241b91eb568a3e061d3a442baaf2
Kirill Makhonin <Kirill_Makhonin@epam.com>
2017-10-19 16:11:51 +0300

    Fix kibana URL and logstash, add logspout

---
+++ logspout/modules.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	_ "github.com/gliderlabs/logspout/adapters/syslog"
+	_ "github.com/gliderlabs/logspout/transports/tcp"
+	_ "github.com/gliderlabs/logspout/transports/tls"
+	_ "github.com/gliderlabs/logspout/transports/udp"
+	_ "github.com/looplab/logspout-logstash"
+)
