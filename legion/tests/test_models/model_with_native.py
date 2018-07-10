4513dd9fc63926a2a0682cfd4534497d347e15be
Kirill Makhonin <kroks.rus@gmail.com>
2018-07-10 15:56:22 +0300

    [#31] Fix encode/decode mistype

--- legion/tests/test_models/model_with_native.py
+++ legion/tests/test_models/model_with_native.py
@@ -11,7 +11,7 @@ installation_result = subprocess.run('apt-get update && apt-get install -y mc',
                                      stdout=subprocess.PIPE)
 if installation_result.returncode != 0:
     raise Exception('Invalid result code {}: \n{}'.format(installation_result.returncode,
-                                                          installation_result.stdout.encode('utf-8')))
+                                                          installation_result.stdout.decode('utf-8')))
 
 
 def calculate(x):
