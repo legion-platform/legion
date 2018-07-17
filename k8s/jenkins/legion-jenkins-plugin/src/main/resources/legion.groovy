dfc0d1d2fa0906800dac3d0b88304d384f15d441
ablatov <a.blatov1990@gmail.com>
2018-07-17 06:51:51 +0300

    [#234] pull requests comment fixes

--- k8s/jenkins/legion-jenkins-plugin/src/main/resources/legion.groovy
+++ k8s/jenkins/legion-jenkins-plugin/src/main/resources/legion.groovy
@@ -1,7 +1,7 @@
 #!groovy
 
 /**
- *   Copyright 2017 EPAM Systems
+ *   Copyright 2018 EPAM Systems
  *
  *   Licensed under the Apache License, Version 2.0 (the "License");
  *   you may not use this file except in compliance with the License.
