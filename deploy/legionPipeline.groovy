113137213023ae9d421fb1f8b8d6c08d137e29dd
ablatov <a.blatov1990@gmail.com>
2018-07-17 19:47:13 +0300

    [#234] add tags from develop

--- deploy/legionPipeline.groovy
+++ deploy/legionPipeline.groovy
@@ -93,9 +93,7 @@ def createjenkinsJobs(String commitID) {
 def runRobotTests(tags="") {
     withAWS(credentials: 'kops') {
     	withCredentials([file(credentialsId: params.Profile, variable: 'CREDENTIAL_SECRETS')]) {
-            env.tags=tags.toString().trim()
-            env.robot_tags= !env.tags.empty ? "-i " + env.tags.replaceAll(',',' -i ') : ""
-            env.nose_tags = !env.tags.empty ? "-a " + env.tags.replaceAll(',',' -a ') : ""
+            env.tags=tags
             sh '''
             cd legion
             ../.venv/bin/pip install -r requirements/base.txt
@@ -121,7 +119,7 @@ def runRobotTests(tags="") {
             kops export kubecfg --name $CLUSTER_NAME --state $CLUSTER_STATE_STORE
             PATH=../../.venv/bin:$PATH DISPLAY=:99 \
             PROFILE=$Profile BASE_VERSION=$BaseVersion LOCAL_VERSION=$LocalVersion \
-            ../../.venv/bin/python3 -m robot.run --variable PATH_TO_PROFILES_DIR:$PATH_TO_PROFILES_DIR $robot_tags *.robot || true
+            ../../.venv/bin/python3 -m robot.run --variable PATH_TO_PROFILES_DIR:$PATH_TO_PROFILES_DIR $tags *.robot || true
 
             echo "Starting python tests"
             cd ../python
