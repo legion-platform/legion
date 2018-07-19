ca34edbfb2735281120fe8bde8972685314910c6
Alexander Semenets <am.semenets@gmail.com>
2018-07-19 11:35:01 +0300

    [#214] Get labels from registry during tests

--- legion/tests/test_k8s.py
+++ legion/tests/test_k8s.py
@@ -27,6 +27,11 @@ import legion.config
 import legion.containers.docker
 import legion.containers.headers
 
+try:
+    from .legion_test_utils import LegionTestContainer
+except ImportError:
+    from legion_test_utils import LegionTestContainer
+
 
 class TestK8S(unittest2.TestCase):
     @classmethod
@@ -46,6 +51,18 @@ class TestK8S(unittest2.TestCase):
         )
 
         return new_image[0]
+    
+    def _push_docker_image_to_registry(self, image, registry):
+        try:
+            docker_client = legion.containers.docker.build_docker_client()
+            image = docker_client.images.get(image)
+            image.tag('{}/{}'.format(registry, image))
+            docker_client.images.push('{}/{}'.format(registry, image))
+            image_url = 
+        except Exception as err:
+            print('Can\'t push image to registry: {}'format(err))
+
+        return image_url
 
     @staticmethod
     def _build_test_model_labels(model_id='id', model_version='1.0', container_type='model'):
@@ -72,22 +89,28 @@ class TestK8S(unittest2.TestCase):
             'abc': 'def',
             'efg': 'ghk'
         }
-        image_name = 'legion/test-image'
-        self._build_bare_docker_image(image_name, labels)
         with self.assertRaises(Exception) as raised_exception:
-            legion.k8s.utils.get_docker_image_labels(image_name)
+            with LegionTestContainer(image='registry', port=5000) as registry_container:
+                labels = self._build_test_model_labels()
+                image_name = 'legion/test-image'
+                self._build_bare_docker_image(image_name, labels)
+                self._push_docker_image_to_registry(image_name, 'localhost:{}'format(registry_container.host_port))
+                image_url = 'http://localhost:{}/{}'.format(registry_container.host_port, image_name)
+                legion.k8s.utils.get_docker_image_labels(image_url)
 
-        self.assertEqual(len(raised_exception.exception.args), 1, 'exception doesnt contain arguments')
-        self.assertTrue(raised_exception.exception.args[0].startswith('Missed one of '), 'wrong exception text')
+                self.assertEqual(len(raised_exception.exception.args), 1, 'exception doesn\'t contain arguments')
+                self.assertTrue(raised_exception.exception.args[0].startswith('Missed one of '), 'wrong exception text')
 
     def test_get_labels_from_docker_image_exception(self):
 
-        labels = self._build_test_model_labels()
-        image_name = 'legion/test-image'
-        self._build_bare_docker_image(image_name, labels)
-
-        received_labels = legion.k8s.utils.get_docker_image_labels(image_name)
-        self.assertDictEqual(received_labels, labels)
+        with LegionTestContainer(image='registry', port=5000) as registry_container:
+            labels = self._build_test_model_labels()
+            image_name = 'legion/test-image'
+            self._build_bare_docker_image(image_name, labels)
+            self._push_docker_image_to_registry(image_name, 'localhost:{}'format(registry_container.host_port))
+            image_url = 'http://localhost:{}/{}'.format(registry_container.host_port, image_name)
+            received_labels = legion.k8s.utils.get_docker_image_labels(image_url)
+            self.assertDictEqual(received_labels, labels)
 
     def test_get_meta_from_docker_labels(self):
         source_model_id = 'abc'
@@ -107,17 +130,6 @@ class TestK8S(unittest2.TestCase):
         for key, value in compatible_labels.items():
             self.assertIsInstance(key, str, 'labels key is not a string')
             self.assertIsInstance(value, str, 'labels key is not a string')
-
-    def test_container_id_detection_kubernetes(self):
-        self.assertEqual(legion.containers.docker.get_docker_container_id_from_cgroup_line(
-            '9:perf_event:/kubepods/burstable/pod9aa3c2f1-8381-11e8-9508-0ae20568e616' +
-            '/8383095d66a50d724da34073b512a468ee565042586a4a7a5bbc741aa426e012'
-        ), '8383095d66a50d724da34073b512a468ee565042586a4a7a5bbc741aa426e012')
-
-    def test_container_id_detection_docker(self):
-        self.assertEqual(legion.containers.docker.get_docker_container_id_from_cgroup_line(
-            '9:devices:/docker/54d998b5f4232277ef245f7d93b0156dec3e149186916c557190983863bc7f57'
-        ), '54d998b5f4232277ef245f7d93b0156dec3e149186916c557190983863bc7f57')
 
 
 if __name__ == '__main__':
