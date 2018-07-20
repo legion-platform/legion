4b2cd993d5520c0bd0750fb12595acdda496d1b4
leshchanka-aliaksandr <leshchanka.aliaksandr@gmail.com>
2018-07-20 15:18:43 +0300

    [#235] debug commit

--- deploy/ansible/library/validator.py
+++ deploy/ansible/library/validator.py
@@ -96,7 +96,7 @@ def main():
     if not file:
         module.fail_json(msg="{} file is empty".format(module.params['source']))
 
-    module.debug(msg="{}".format(file))
+    module.log(msg="{}".format(file))
     data = validate(file, template)
 
     if data:
