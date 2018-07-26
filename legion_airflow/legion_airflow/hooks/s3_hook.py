ceb27f66bc11f11be279dffce1c68fd5497fbbfb
Aliaksandr Dunayeu <aliaksandr_dunayeu@epam.com>
2018-07-26 21:31:55 +0300

    [#270] Fixed issue with None in bucket name

--- legion_airflow/legion_airflow/hooks/s3_hook.py
+++ legion_airflow/legion_airflow/hooks/s3_hook.py
@@ -72,7 +72,7 @@ class S3Hook(BaseHook):
         :return: s3 file
         """
         self.check_if_maintenance(bucket, key)
-        uri = 's3://{}{}/{}{}'.format(self.bucket_prefix, bucket, self.key_prefix, key)
+        uri = 's3://{}{}/{}{}'.format(self.bucket_prefix or '', bucket or '', self.key_prefix or '', key)
         return smart_open.smart_open(uri=uri, mode=mode,
                                      encoding=encoding,
                                      aws_access_key_id=self.aws_access_key_id,
