2e3a022ecd1aea45cdda0a6aa00b2bd934f5c08d
Aleksandr Blatov <a.blatov1990@gmail.com>
2018-09-04 09:35:02 +0300

    [#379] Fixes incorrect auth urls for components security check (#380)

--- legion_test/legion_test/robot/utils.py
+++ legion_test/legion_test/robot/utils.py
@@ -167,23 +167,27 @@ class Utils:
             raise Exception('Unexpected case happen!')
 
     @staticmethod
-    def get_component_auth_page(url):
+    def get_component_auth_page(url, jenkins=False):
         """
         Get component main auth page
 
-        :param url: component url
-        :type url: str
+        :param boolean jenkins: if jenkins service is under test
+        :param str url: component url
         :return:  response_code and response_text
         :rtype: dict
         """
-        response = requests.get(url, timeout=10)
+        if jenkins:
+            response = requests.get('{}/securityRealm/commenceLogin'.format(url), timeout=10)
+        else:
+            response = requests.get(url, timeout=10)
         return {"response_code": response.status_code, "response_text": response.text}
 
     @staticmethod
-    def post_credentials_to_auth(component_url, cluster_host, creds):
+    def post_credentials_to_auth(component_url, cluster_host, creds, jenkins=False):
         """
-        Send post request with credentials to authorize
+        Get session id and send post request with credentials to authorize
 
+        :param boolean jenkins: if jenkins service is under test
         :param str component_url: legion core component url
         :param str cluster_host: cluster host name
         :param dict creds: dict with login and password
@@ -194,7 +198,11 @@ class Utils:
         import re
 
         session = requests.Session()
-        response = session.get("{}.{}/securityRealm/commenceLogin".format(component_url, cluster_host))
+        if jenkins:
+            response = session.get("{}.{}/securityRealm/commenceLogin".format(component_url, cluster_host))
+        else:
+            response = session.get("{}.{}".format(component_url, cluster_host))
+
         if response.status_code != 200:
             raise IOError('Authentication endpoint is unavailable, got {} http code'
                           .format(response.status_code))
@@ -208,8 +216,7 @@ class Utils:
         if creds["login"] == "admin":
             response = session.post(AUTHENTICATION_PATH.format(cluster_host, request_id), creds)
         else:
-            session.post(AUTHENTICATION_PATH.format(cluster_host, request_id), creds)
-            response = session.get("{}.{}".format(component_url, cluster_host))
+            response = session.post(AUTHENTICATION_PATH.format(cluster_host, request_id), creds)
         return {"response_code": response.status_code, "response_text": response.text}
 
     @staticmethod
