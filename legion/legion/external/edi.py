85f65a3cd8a8359985a42a2d30c05be166ce7573
Alexander D <alexd.by@gmail.com>
2018-09-05 16:57:23 +0300

    [#102] Fixed code style in edi client

--- legion/legion/external/edi.py
+++ legion/legion/external/edi.py
@@ -281,7 +281,6 @@ def build_client(args=None):
     :type args: :py:class:`argparse.Namespace`
     :return: :py:class:`legion.external.edi.EdiClient` -- EDI client
     """
-
     host, user, password, token = None, None, None, None
 
     if args:
