45a14779750e84742af6a6736d76c783eae404d1
Aliaksandr Leshchanka <leshchanka.aliaksandr@gmail.com>
2018-10-25 13:09:51 +0300

    [#525] update plugin and nginx template

--- legion/legion/template_plugins/k8s.py
+++ legion/legion/template_plugins/k8s.py
@@ -25,7 +25,7 @@ from legion.k8s import K8SConfigMapStorage
 LOGGER = logging.getLogger(__name__)
 
 
-async def enclave_configmap_monitor(template_system, var_name="var"):
+async def enclave_configmap_monitor(template_system, var_name="var", key=""):
     """
     Update template with current model state
 
@@ -38,10 +38,9 @@ async def enclave_configmap_monitor(template_system, var_name="var"):
     config_map = K8SConfigMapStorage.retrive(
         storage_name="legion-{}-invalid-tokens".format(namespace),
         k8s_namespace=namespace)
-    config_map.load()
     LOGGER.info("initial value is {}".format(config_map.data))
 
     for event, new_state in config_map.watch():
         LOGGER.info('Got updated configmap state')
         LOGGER.info("New state is {}".format(new_state))
-        template_system.render(**{var_name: new_state})
+        template_system.render(**{var_name: new_state.get(key, "")})
