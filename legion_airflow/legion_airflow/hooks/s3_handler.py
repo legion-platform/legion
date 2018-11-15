c4ebefcbe1f8b02e78bc72b7562098e78ad3543f
Alexander D <alexd.by@gmail.com>
2018-11-15 12:29:37 +0300

    [#270] Fixed S3Hook to work without connection_id

--- legion_airflow/legion_airflow/hooks/s3_handler.py
+++ legion_airflow/legion_airflow/hooks/s3_handler.py
@@ -24,14 +24,13 @@ class S3TaskHandlerWithIAM(S3TaskHandler):
     """
 
     def _build_hook(self):
-        remote_conn_id = conf.conf.get('core', 'REMOTE_LOG_CONN_ID')
         try:
             from legion_airflow.hooks.s3_hook import S3Hook
-            return S3Hook(remote_conn_id)
+            return S3Hook()
         except Exception as e:
             self.log.error(
-                'Could not create an S3Hook with connection id "%s". '
+                'Could not create an S3Hook connection. '
                 'Please make sure that airflow[s3] is installed and '
-                'the S3 connection exists.', remote_conn_id
+                'the S3 connection exists.'
             )
             raise
