921116fc9c3f64e3092c688b031a4a466c1d0b7a
Alexander Semenets <am.semenets@gmail.com>
2018-11-26 12:05:50 +0300

    [#566] set ansible image full path

--- deploy/legionPipeline.groovy
+++ deploy/legionPipeline.groovy
@@ -34,7 +34,7 @@ def createCluster() {
     file(credentialsId: "vault-${env.param_profile}", variable: 'vault')]) {
         withAWS(credentials: 'kops') {
             wrap([$class: 'AnsiColorBuildWrapper', colorMapName: "xterm"]) {
-                docker.image("${env.param_docker_repo}/k8s-ansible:${env.param_legion_version}").inside("-e HOME=/opt/legion/deploy -v ${WORKSPACE}/deploy/profiles:/opt/legion/deploy/profiles -v /etc/ssl:/etc/ssl -u root") {
+                docker.image("${env.param_docker_repo}/k8s-ansible:${env.param_legion_version}").inside("-e HOME=/opt/legion/deploy -v ${WORKSPACE}/deploy/profiles:/opt/legion/deploy/profiles -u root") {
                     stage('Create cluster') {
                         sh """
                         cd ${ansibleHome} && \
