55a00a6c28cfa2d71df3b369a280e7eeb4182ba7
Kirill Makhonin <kroks.rus@gmail.com>
2018-12-27 15:39:38 +0300

    [#313] Add Jinja

--- legion/legion/edi/server.py
+++ legion/legion/edi/server.py
@@ -24,16 +24,21 @@ import legion.k8s
 import legion.external.grafana
 import legion.http
 import legion.model
-from flask import Flask, Blueprint
+from flask import Flask, Blueprint, render_template
 from flask import current_app as app
+import os
 import jwt
 from datetime import datetime, timedelta
 
 LOGGER = logging.getLogger(__name__)
 blueprint = Blueprint('apiserver', __name__)
+TEMPLATES_FOLDER = os.path.abspath(os.path.join(
+    os.path.dirname(__file__), os.pardir, 'templates', 'edi')
+)
 
 EDI_VERSION = '1.0'
 EDI_ROOT = '/'
+EDI_API_ROOT = '/api/'
 EDI_DEPLOY = '/api/{version}/deploy'
 EDI_UNDEPLOY = '/api/{version}/undeploy'
 EDI_SCALE = '/api/{version}/scale'
@@ -91,9 +96,18 @@ def return_model_deployments(model_deployments):
 
 
 @blueprint.route(build_blueprint_url(EDI_ROOT), methods=['GET'])
+def root():
+    """
+    Root endpoint for authorisation
+
+    :return: dict -- root information
+    """
+    return render_template('index.html')
+
+@blueprint.route(build_blueprint_url(EDI_API_ROOT), methods=['GET'])
 @legion.http.provide_json_response
 @legion.http.authenticate(authenticate)
-def root():
+def api_root():
     """
     Root API endpoint
 
@@ -190,7 +204,7 @@ def undeploy(model, version=None, grace_period=0, ignore_not_found=False):
 
 
     :param model: model id
-    :type model: str
+    :type model: strset jinja folder
     :param version: (Optional) specific model version
     :type version: str or None
     :param grace_period: grace period for removing
@@ -351,13 +365,16 @@ def generate_token(model_id, model_version):
 
 def create_application():
     """
-    Create Flask application and register blueprints
+    Create Flask application, register blueprints and templates folder
 
     :return: :py:class:`Flask.app` -- Flask application instance
     """
-    application = Flask(__name__, static_url_path='')
+    application = Flask(__name__,
+                        static_url_path='',
+                        template_folder=TEMPLATES_FOLDER)
 
     application.register_blueprint(blueprint)
+    application.create_jinja_environment()
 
     return application
 
@@ -400,7 +417,7 @@ def load_cluster_config(application):
 
     application.config['ENCLAVE'] = get_application_enclave(application)
     application.config['CLUSTER_SECRETS'] = legion.k8s.load_secrets(application.config['CLUSTER_SECRETS_PATH'])
-    application.config['GRAFANA_CLIENT'] = get_application_grafana(application)
+    # application.config['GRAFANA_CLIENT'] = get_application_grafana(application)
     application.config['JWT_CONFIG'] = legion.k8s.load_secrets(application.config['JWT_CONFIG_PATH'])
 
 
