1e1490ef7676c6c86645e9578def71bd8d8d3fe6
Kirill Makhonin <kroks.rus@gmail.com>
2018-12-29 13:15:43 +0300

    [#710] Verbose logging

--- k8s/edge/request_id_generator.lua
+++ k8s/edge/request_id_generator.lua
@@ -15,7 +15,7 @@
 --
 local os = require("os")
 local uuid = require "resty.jit-uuid"
-local str = require "resty.string"
+local bit = require 'bit'
 local _M = {}
 
 
@@ -40,23 +40,27 @@ function _M.reset_seed()
     --]]
 
     -- First seed word
-    local seed_p1 = bit.blshift(32, pod_uuid_crc32)         -- CRC32(POD_ID) << 32
+    local seed_p1 = bit.lshift(32, pod_uuid_crc32)         -- CRC32(POD_ID) << 32
     -- Second seed word
-    local seed_p2_pid = bit.blshift(28, bit.band(pid, 0xF)) -- (PID & 0xF) << 28
-    local seed_p2_wid = bit.blshift(24, bit.band(wid, 0xF)) -- (WID & 0xF) << 24
+    local seed_p2_pid = bit.lshift(28, bit.band(pid, 0xF)) -- (PID & 0xF) << 28
+    local seed_p2_wid = bit.lshift(24, bit.band(wid, 0xF)) -- (WID & 0xF) << 24
     local seed_p2_time = bit.band(time, 0x0FFFFFFF)         -- TIMESTAMP & 0x0FFFFFFF
     local seed_p2 = bit.bor(bit.bor(seed_p2_pid, seed_p2_wid), seed_p2_time)
     -- Entire seed
     local seed = bit.bor(seed_p1, seed_p2)
 
-    ngx.log(ngx.ERR, "Resetting UUID seed to 0x"..str.to_hex(seed))
+    ngx.log(ngx.ERR, "Resetting UUID seed to 0x"..seed)
 
     math.randomseed(seed)
 end
 
 function _M.generate_request_id()
+    local wid = ngx.worker.id() -- worker ID
+    ngx.log(ngx.ERR, "Generating request id on worker "..wid)
     return uuid()
 end
 
 -- Reset seed on load
 _M.reset_seed()
+
+return _M
