65d23091d41d355543dc747400d5f68d273486e7
Kirill Makhonin <kroks.rus@gmail.com>
2019-03-21 15:05:59 +0300

    [#823] Cleanup code

--- legion-operator/pkg/controller/modeltraining/modeltraining_controller.go
+++ legion-operator/pkg/controller/modeltraining/modeltraining_controller.go
@@ -98,57 +98,48 @@ type ReconcileModelTraining struct {
 	recorder record.EventRecorder
 }
 
-// Reconcile reads that state of the cluster for a ModelTraining object and makes changes based on the state read
-// and what is in the ModelTraining.Spec
-// +kubebuilder:rbac:groups=core,resources=pods,verbs=get;list;watch;create;update;patch;delete
-// +kubebuilder:rbac:groups=core,resources=pods/status,verbs=get;update;patch
-// +kubebuilder:rbac:groups=core,resources=secrets,verbs=get;list;watch;create;update;patch;delete
-// +kubebuilder:rbac:groups=legion.legion-platform.org,resources=modeltrainings,verbs=get;list;watch;create;update;patch;delete
-// +kubebuilder:rbac:groups=legion.legion-platform.org,resources=modeltrainings/status,verbs=get;update;patch
-// +kubebuilder:rbac:groups=legion.legion-platform.org,resources=vcscredentials,verbs=get;list
-// +kubebuilder:rbac:groups="",resources=events,verbs=create;patch
-func (r *ReconcileModelTraining) Reconcile(request reconcile.Request) (reconcile.Result, error) {
-	// Fetch the ModelTraining instance
-	instance := &legionv1alpha1.ModelTraining{}
+const (
+	secretNameTemplate  = "%s-training-secret"
+	podNameTemplate     = "%s-training-pod"
+	gitSecretVolumeName = "git-secrets"
+	gitSecretPath       = "/home/root/.ssh"
+)
 
-	if err := r.Get(context.TODO(), request.NamespacedName, instance); err != nil {
-		if errors.IsNotFound(err) {
-			log.Error(err, "Cannot find CR instance for event")
-			// Object not found, return.  Created objects are automatically garbage collected.
-			// For additional cleanup logic use finalizers.
-			return reconcile.Result{}, nil
-		}
-		log.Error(err, "Cannot fetch CR status")
-		return reconcile.Result{}, err
-	}
+// FindVCSInstance finds relevant VCS instance for ModelTraining instance
+func FindVCSInstance(r *ReconcileModelTraining, instance *legionv1alpha1.ModelTraining, vcsInstance *legionv1alpha1.VCSCredential) error {
+	instanceLog := log.WithValues("mt-namespace", instance.Namespace, "mt-name", instance.Name)
 
-	vcsInstance := &legionv1alpha1.VCSCredential{}
 	vcsInstanceName := types.NamespacedName{
 		Name:      instance.Spec.VCSName,
-		Namespace: request.NamespacedName.Namespace,
+		Namespace: instance.Namespace,
 	}
 
 	if err := r.Get(context.TODO(), vcsInstanceName, vcsInstance); err != nil {
-		log.Error(err, "Cannot fetch VCS Credential with name", vcsInstanceName)
+		instanceLog.Error(err, "Cannot fetch VCS Credential with name", vcsInstanceName)
 		r.recorder.Event(instance, "Warning", "CannotFetch",
 			fmt.Sprintf("Cannot find VCS Credential %s. Updation ignored", vcsInstanceName))
 
 		// Error reading the object - requeue the request.
-		return reconcile.Result{}, err
+		return err
 	}
 
-	// Define secret if it is not null
-	var (
-		secret          *corev1.Secret
-		secretName      = instance.Name + "-training-secret"
-		gitSecretVolume = "git-key"
-		podName         = instance.Name + "-training-pod"
-	)
+	return nil
+}
+
+// BuildK8SItems builds Pod and Secret for ModelTraining instance
+func BuildK8SItems(r *ReconcileModelTraining, instance *legionv1alpha1.ModelTraining, pod *corev1.Pod, secret *corev1.Secret) error {
+	instanceLog := log.WithValues("mt-namespace", instance.Namespace, "mt-name", instance.Name)
+
+	vcsInstance := &legionv1alpha1.VCSCredential{}
+
+	if err := FindVCSInstance(r, instance, vcsInstance); err != nil {
+		return err
+	}
 
 	if vcsInstance.Spec.VCSCreds != "" {
-		secret = &corev1.Secret{
+		*secret = corev1.Secret{
 			ObjectMeta: metav1.ObjectMeta{
-				Name:      secretName,
+				Name:      fmt.Sprintf(secretNameTemplate, instance.Name),
 				Namespace: instance.Namespace,
 			},
 			Data: map[string][]byte{
@@ -157,15 +148,15 @@ func (r *ReconcileModelTraining) Reconcile(request reconcile.Request) (reconcile
 		}
 	}
 
-	pod := &corev1.Pod{
+	*pod = corev1.Pod{
 		ObjectMeta: metav1.ObjectMeta{
-			Name:      podName,
+			Name:      fmt.Sprintf(podNameTemplate, instance.Name),
 			Namespace: instance.Namespace,
 		},
 		Spec: corev1.PodSpec{
 			Containers: []corev1.Container{
 				{
-					Name:  "nginx",
+					Name:  "training",
 					Image: "nginx",
 				},
 			},
@@ -176,86 +167,105 @@ func (r *ReconcileModelTraining) Reconcile(request reconcile.Request) (reconcile
 	if secret != nil {
 		// Add volume
 		pod.Spec.Volumes = append(pod.Spec.Volumes, corev1.Volume{
-			Name: gitSecretVolume,
+			Name: gitSecretVolumeName,
 			VolumeSource: corev1.VolumeSource{
 				Secret: &corev1.SecretVolumeSource{ //+safeToCommit
-					SecretName: secretName,
+					SecretName: fmt.Sprintf(secretNameTemplate, instance.Name),
 				},
 			},
 		})
 
 		// Add volume mount
 		pod.Spec.Containers[0].VolumeMounts = append(pod.Spec.Containers[0].VolumeMounts, corev1.VolumeMount{
-			Name:      gitSecretVolume,
+			Name:      gitSecretVolumeName,
 			ReadOnly:  true,
-			MountPath: "/home/root/.ssh",
+			MountPath: gitSecretPath,
 		})
 
 		if err := controllerutil.SetControllerReference(instance, secret, r.scheme); err != nil {
-			log.Error(err, "Cannot attach owner info to Secret")
-			return reconcile.Result{}, err
+			instanceLog.Error(err, "Cannot attach owner info to Secret")
+			return err
 		}
 	}
 
 	if err := controllerutil.SetControllerReference(instance, pod, r.scheme); err != nil {
 		log.Error(err, "Cannot attach owner info to Pod")
-		return reconcile.Result{}, err
+		return err
 	}
 
-	// All resources have been pre builded
-	// Creation phase
+	return nil
+}
 
-	foundSecret := &corev1.Secret{}
+// SyncronizeK8SInstances updates K8S instances to match desired state
+func SyncronizeK8SInstances(r *ReconcileModelTraining, instance *legionv1alpha1.ModelTraining, pod *corev1.Pod, secret *corev1.Secret) (bool, error) {
+	instanceLog := log.WithValues("mt-namespace", instance.Namespace, "mt-name", instance.Name)
+
+	anyInstanceCreated := false
+	foundSecret := corev1.Secret{}
+	foundPod := corev1.Pod{}
+
 	// Create secret or fail (if it is not created yet)
 	if secret != nil {
 		secretNamespacedName := types.NamespacedName{Name: secret.Name, Namespace: secret.Namespace}
-		if err := r.Get(context.TODO(), secretNamespacedName, foundSecret); err != nil && errors.IsNotFound(err) {
-			log.Info("Creating SVC secret", secretNamespacedName)
+		if err := r.Get(context.TODO(), secretNamespacedName, &foundSecret); err != nil && errors.IsNotFound(err) {
+			// If can not find Secret
+			instanceLog.Info("Creating SVC secret", "name", secretNamespacedName)
 			err = r.Create(context.TODO(), secret)
-			if err == nil {
-				log.Info("Secret created")
-				r.recorder.Event(instance, "Normal", "Spawning",
-					fmt.Sprintf("Creating Secret %s", secretNamespacedName))
-			} else {
-				log.Error(err, "Cannot create Secret")
+			if err != nil {
+				// If cannot create secret
+				instanceLog.Error(err, "Cannot create Secret")
 				r.recorder.Event(instance, "Warning", "CannotSpawn",
 					fmt.Sprintf("Cannot create Secret %s. Failed", secretNamespacedName))
-				return reconcile.Result{}, err
+				return anyInstanceCreated, err
 			}
+
+			instanceLog.Info("Secret created")
+			r.recorder.Event(instance, "Normal", "Spawning",
+				fmt.Sprintf("Creating Secret %s", secretNamespacedName))
+			*secret = foundSecret
+			anyInstanceCreated = true
+
 		} else if err != nil {
-			log.Error(err, "Cannot fetch secrets")
-			return reconcile.Result{}, err
+			instanceLog.Error(err, "Cannot fetch secrets")
+			return anyInstanceCreated, err
 		}
 	}
-
-	foundPod := &corev1.Pod{}
 
 	podNamespacedName := types.NamespacedName{Name: pod.Name, Namespace: pod.Namespace}
 	// Create pod & return or fail (if it is not created yet)
-	if err := r.Get(context.TODO(), podNamespacedName, foundPod); err != nil && errors.IsNotFound(err) {
-		log.Info("Creating pod", podNamespacedName)
+	if err := r.Get(context.TODO(), podNamespacedName, &foundPod); err != nil && errors.IsNotFound(err) {
+		// If can not find Pod
+		instanceLog.Info("Creating pod", podNamespacedName)
 
 		err = r.Create(context.TODO(), pod)
-		if err == nil {
-			log.Info("Created Pod")
-			r.recorder.Event(instance, "Normal", "Spawning",
-				fmt.Sprintf("Creating Pod %s", podNamespacedName))
-			return reconcile.Result{}, err
+		if err != nil {
+			// If error during Pod creation
+			instanceLog.Error(err, "Cannot create Pod")
+			r.recorder.Event(instance, "Warning", "CannotSpawn",
+				fmt.Sprintf("Cannot create Pod %s. Failed", podNamespacedName))
+			return anyInstanceCreated, err
 		}
 
-		log.Error(err, "Cannot create Pod")
-		r.recorder.Event(instance, "Warning", "CannotSpawn",
-			fmt.Sprintf("Cannot create Pod %s. Failed", podNamespacedName))
-		return reconcile.Result{}, err
-
+		// If no errors during Pod creation
+		instanceLog.Info("Created Pod")
+		r.recorder.Event(instance, "Normal", "Spawning",
+			fmt.Sprintf("Creating Pod %s", podNamespacedName))
+		anyInstanceCreated = true
+		*pod = foundPod
 	} else if err != nil {
-		log.Error(err, "Cannot fetch actual pods")
-		return reconcile.Result{}, err
+		// If cannot gather Pod information
+		instanceLog.Error(err, "Cannot fetch actual pods")
+		return anyInstanceCreated, err
 	}
 
-	// OK - instances syncronized. Now we can check & update statuses
+	return anyInstanceCreated, nil
+}
 
-	switch foundPod.Status.Phase {
+// SyncronizeK8SState syncronize state of ModelTraining instance based on child instances
+func SyncronizeK8SState(r *ReconcileModelTraining, instance *legionv1alpha1.ModelTraining, pod *corev1.Pod, secret *corev1.Secret) error {
+	instanceLog := log.WithValues("mt-namespace", instance.Namespace, "mt-name", instance.Name)
+
+	switch pod.Status.Phase {
 	case corev1.PodPending:
 		instance.Status.TrainingState = legionv1alpha1.ModelTrainingScheduling
 	case corev1.PodRunning:
@@ -269,7 +279,70 @@ func (r *ReconcileModelTraining) Reconcile(request reconcile.Request) (reconcile
 	}
 
 	if err := r.Update(context.TODO(), instance); err != nil {
-		log.Error(err, "Cannot update status of CR instance")
+		instanceLog.Error(err, "Cannot update status of CR instance")
+		return err
+	}
+
+	return nil
+}
+
+// Reconcile reads that state of the cluster for a ModelTraining object and makes changes based on the state read
+// and what is in the ModelTraining.Spec
+// +kubebuilder:rbac:groups=core,resources=pods,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=core,resources=pods/status,verbs=get;update;patch
+// +kubebuilder:rbac:groups=core,resources=secrets,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=legion.legion-platform.org,resources=modeltrainings,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=legion.legion-platform.org,resources=modeltrainings/status,verbs=get;update;patch
+// +kubebuilder:rbac:groups=legion.legion-platform.org,resources=vcscredentials,verbs=get;list
+// +kubebuilder:rbac:groups="",resources=events,verbs=create;patch
+func (r *ReconcileModelTraining) Reconcile(request reconcile.Request) (reconcile.Result, error) {
+	instanceLog := log.WithValues("mt-namespace", request.Namespace, "mt-name", request.Name)
+
+	// Fetch the ModelTraining instance
+	instance := &legionv1alpha1.ModelTraining{}
+
+	if err := r.Get(context.TODO(), request.NamespacedName, instance); err != nil {
+		if errors.IsNotFound(err) {
+			instanceLog.Error(err, "Cannot find CR instance for event")
+			// Object not found, return.  Created objects are automatically garbage collected.
+			// For additional cleanup logic use finalizers.
+			return reconcile.Result{}, nil
+		}
+		instanceLog.Error(err, "Cannot fetch CR status")
+		return reconcile.Result{}, err
+	}
+
+	// Define secret if it is not null
+	var (
+		pod    = &corev1.Pod{}
+		secret = &corev1.Secret{}
+	)
+
+	if err := BuildK8SItems(r, instance, pod, secret); err != nil {
+		instanceLog.Error(err, "Cannot build appropriate pod/secret state for model training instance")
+
+		r.recorder.Event(instance, "Warning", "CannotProcess",
+			"Cannot build appropriate pod/secret state. Updation ignored")
+
+		return reconcile.Result{}, err
+	}
+
+	// Creation phase
+	if created, err := SyncronizeK8SInstances(r, instance, pod, secret); err != nil {
+		instanceLog.Error(err, "Can not syncronize desired K8S instances state to cluster")
+		if created {
+			instanceLog.Error(err, "Looks like some (but not all) resources have been created")
+		}
+		return reconcile.Result{}, err
+	} else if created {
+		instanceLog.Info("All resources has been created")
+		return reconcile.Result{}, nil
+	}
+
+	// OK - instances syncronized. Now we can check & update statuses
+
+	if err := SyncronizeK8SState(r, instance, pod, secret); err != nil {
+		instanceLog.Error(err, "Can't syncronize K8S state")
 		return reconcile.Result{}, err
 	}
 
